25a9abc32c677ae1b5254cd0b586fbd38e9707fa
Ryusei <tsubasato26@gmail.com>
2026-03-21 19:56:43 -0700

    deploy fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://uogsjypcvdisujzeremr.supabase.co"
-const supabaseKey = "sb_publishable_SO1xjwssmA27_EXeDpuGCA_UdlGyFSQ"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-)
+export const supabase = createClient(supabaseUrl, supabaseKey)
